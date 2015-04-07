03d414f63e70f89ccabf0829ab479e81fd8ddd3e
John Stowers <john.stowers@gmail.com>
2015-04-07 14:12:23 +0200

    python: maxlen is configurable

--- src/arduinoudev.py
+++ src/arduinoudev.py
@@ -1,3 +1,4 @@
+import time
 import serial
 
 def _crc8maxim(crc, c):
@@ -15,15 +16,18 @@ def crc8maxim(s):
         crc = _crc8maxim(crc,c)
     return crc
 
-def is_valid_name(name):
-    return all([n.isalnum() for n in name]) and len(name) <= 8
+def is_valid_name(name,maxlen=8):
+    return all([n.isalnum() for n in name]) and len(name) <= maxlen
 
-def get_device_name(device):
+def open_device(port):
+    return serial.Serial(port, baudrate=9600, timeout=10.)
+
+def get_device_name(device, maxlen=8):
     device.write('N?')
 
-    name_and_crc = device.read(10)
-    name = name_and_crc[:8]
-    crc = name_and_crc[8:]
+    name_and_crc = device.read(maxlen+2)
+    name = name_and_crc[:maxlen]
+    crc = name_and_crc[maxlen:]
 
     #check the name contains some ascii
     some_ascii = [n.isalnum() for n in name]
@@ -37,13 +41,23 @@ def get_device_name(device):
     else:
         raise ValueError("Name not set: %r" % name)
 
-
-def set_device_name(device,name):
+def set_device_name(device,name,maxlen=8,verbose=False):
     if not is_valid_name(name):
         raise ValueError("Invalid name")
     
     #right padd with NULL
-    padded_name = ''.join([name[i] if i < len(name) else '\0' for i in range(8)])
+    padded_name = ''.join([name[i] if i < len(name) else '\0' for i in range(maxlen)])
+
+    s = 'N='
+    s += padded_name
+    s += '%X'%crc8maxim(padded_name)
+    device.write(s)
+    if verbose:
+        print "RAW: ",
+        for c in s:
+            print "0x%X" % ord(c),
+        print
+
 
     device.write('N=')
     device.write(padded_name)
