bdf20eeb7a7f6f55fc65d3fa0b3df38aa72b259d
Andrew Straw <strawman@astraw.com>
2018-12-06 16:45:20 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='arduino-udev',
+      version='0.2.4',
+      py_modules=['arduinoudev'],
+      package_dir = {'': 'src'},
+      url = "https://github.com/strawlab/arduino-udev",
+      maintainer = "Andrew Straw",
+      maintainer_email = "strawman@astraw.com",
+      )
+
