8a4e97ee14ba8b6097bcb9f69d95519673ae22a4
Gabor Szabo <gabor@szabgab.com>
2020-06-14 16:44:25 +0300

    add name of author

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     name="arduino-udev",
     description="Get and set information by querying serial devices",
     long_description=long_description,
+    author="John Stowers",
     long_description_content_type="text/markdown",
     version="0.2.5",
     py_modules=["arduinoudev"],
