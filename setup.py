9408369894440ec814ff9286bc6cebaedea5ea4d
Andrew Straw <strawman@astraw.com>
2020-06-14 10:56:55 +0200

    set long description from README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='arduino-udev',
+      description="Get and set information by querying serial devices",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       version='0.2.4',
       py_modules=['arduinoudev'],
       package_dir = {'': 'src'},
